212de5729485b39fc6ad99738d890b46699b8635
thisissia <thisissia@gmail.com>
2019-08-29 03:53:44 +0100

    db deploy

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hiiiiii'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://microsite:''@88.80.186.99/microsite'
+    SQLALCHEMY_DATABASE_URI = 'mysql://mysql:720a706c4f6c9bac@dokku-mysql-microsite:3306/microsite'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
